8724ec72ce58a14089d430ded5a3037fc27c1270
Ekiraya <cuentaderegistros12307@gmail.com>
2026-06-29 12:53:56 -0500

    Add files via upload

---
+++ Bulk command execution.ps1
@@ -0,0 +1,24 @@
+$selected_vms = Get-AzVM | Out-GridView -OutputMode Multiple
+
+$script = @"
+
+gpupdate /force
+
+"@
+
+foreach($vm in $selected_vms){
+
+    Write-Host "Currently working on VM:" $vm.Name 
+
+    $reply = Invoke-AzVMRunCommand `
+    -VMName $vm.Name `
+    -ResourceGroupName $vm.ResourceGroupName `
+    -ScriptString $script `
+    -CommandId "RunPowerShellScript"
+
+    $reply.Value[0].Message
+
+    Write-Host "---------------------------"
+
+}
+
